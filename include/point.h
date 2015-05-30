02209afc4931c6d4d729c5d2f1e3f3e29095d7ac
ma1t05 <maltosla@gmail.com>
2015-05-30 15:59:53 -0500

    adding struct for count number of comparations and distance evaluations

--- include/point.h
+++ include/point.h
@@ -3,10 +3,17 @@
 #define _POINT_H_
 
 struct point {
-  float x,y;
+  float x,y; /* x and y coords of point */
+};
+
+struct iterations {
+  struct point *ans;
+  int cont_comp; /* Cantidad de Comparaciones */
+  int cont_dist; /* Cantidad de calculos de distancia */
 };
 
 typedef struct point point;
+typedef struct iterations iterations;
 
 float dist(point*,point*);
 int read_points_file(const char*,point**);
