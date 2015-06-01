9e8e358772141a392d6588832ba67d735606943d
ma1t05 <maltosla@gmail.com>
2015-05-31 21:38:50 -0500

    remove function to anoter library

--- src/mapa.c
+++ src/mapa.c
@@ -1,6 +1,4 @@
 
-#include <stdlib.h>
-#include <stdio.h>
 #include "mapa.h"
 
 cuadrante* _cuadrante_crea(float,float,float,float,int);
@@ -167,18 +165,6 @@ point* cuadrante_search(cuadrante* c,point *p) {
       r = closest_point(c->points,p);
     }
   }
-  return r;
-}
-
-point* closest_point(arrdin* points,point *p){
-  int i;
-  point *q,*r = NULL;
-  for (i = 0;i < points->size;i++) {
-    if (points->a[i] != NULL) {
-      q = (point*)points->a[i];
-      if (r == NULL || dist(p,q) < dist(p,r)) r = q;
-    }
-  }  
   return r;
 }
 
